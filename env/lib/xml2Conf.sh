118c7ce52febaf0f2298948a2de568b54b809758
OuardiFatiha <ouardiifatiha@gmail.com>
2020-10-24 20:05:13 +0200

    hands on pandas 1

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/fatiha/Desktop/Folders/ml_sample_project/env/lib"
+XML2_LIBS="-lxml2 -L/home/fatiha/Desktop/Folders/ml_sample_project/env/lib -lz -L/home/fatiha/Desktop/Folders/ml_sample_project/env/lib -llzma -lpthread -L/home/fatiha/Desktop/Folders/ml_sample_project/env/lib -L/home/fatiha/Desktop/Folders/ml_sample_project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/fatiha/Desktop/Folders/ml_sample_project/env/include/libxml2 -I/home/fatiha/Desktop/Folders/ml_sample_project/env/include"
+MODULE_VERSION="xml2-2.9.10"
+
